6ec665b9a2af7bd7b3d45d010cdf53b0be222089
VENUS LOW <VENUS05.22@ichat.sp.edu.sg>
2023-05-03 10:01:32 +0800

    added submodule

--- bmi.py
+++ bmi.py
@@ -9,10 +9,13 @@ def calculate_bmi(height, weight):
 def weight_classification(bmi):
     if bmi < 18.5:
         print("under weight")
+        return -1
     elif bmi >= 18.5 and bmi <= 25.0:
         print("normal weight")
+        return 0
     else:
         print("over weight")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
 
